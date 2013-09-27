6173d42452822ba68a5e4139146ab951d5c8072c
dansouza <dansouza@cd46cd7e-7c3c-4829-9a36-9a7bc2eed6a4>
2013-09-27 10:47:28 +0000

    Alteração na implementação da Fila encadeada

--- trunk/arduinoCom/src/filaencadeadajava/FilaEncadeada.java
+++ trunk/arduinoCom/src/filaencadeadajava/FilaEncadeada.java
@@ -1,34 +1,36 @@
 /**
- * 
- */
-package filaencadeadajava;
-
-/**
  * @author Leonardo Maxwell
  *
  */
-public class FilaEncadeada{
+
+package filaencadeadajava;
+
+public class FilaEncadeada {
+	private Elemento prim;
+	private Elemento fim;
 	
-	private int num;
-	private FilaEncadeada prox;
-	
-	public int getNum() {
-		return num;
+	public Boolean isEmpty() {
+		return prim == null;
 	}
-	public void setNum(int num) {
-		this.num = num;
-	}
-	public FilaEncadeada getProx() {
-		return prox;
-	}
-	public void setProx(FilaEncadeada prox) {
-		this.prox = prox;
-	}
-	
-	public FilaEncadeada(int num, FilaEncadeada prox) {
-		this.num = num;
-		this.prox = prox;
+	public void enfileira(Elemento n){
+		if(isEmpty()){
+			prim = n;
+			fim = n;
+		}else{
+			fim.setProx(n);
+		}
+		
 	}
 	
+	public Elemento desenfileira(){
+		if(!isEmpty()){
+			prim = prim.getProx();
+		}
+		return prim;
+	}
+	
+	public Elemento verInicio(){
+		return prim;
+	}
 }
 

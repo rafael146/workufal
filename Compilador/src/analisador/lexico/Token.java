c600f13449352b9a48a5b85288278b3de5c934a0
Alisson Oliveira <Alisson Oliveira@cd46cd7e-7c3c-4829-9a36-9a7bc2eed6a4>
2014-06-06 11:15:43 +0000

    adicionado palavras reservadas

--- Compilador/src/analisador/lexico/Token.java
+++ Compilador/src/analisador/lexico/Token.java
@@ -1,5 +1,44 @@
 package analisador.lexico;
 
-public enum Token {
+public class Token {
+	
+	protected TokenType tipo;
+	protected int linha;
+	protected int coluna;
+	protected Object valor;
+	protected boolean temValor;
+	
+	
+	public Token(TokenType tipo, int linha, int coluna) {
+		this.tipo = tipo;
+		this.linha = linha;
+		this.coluna = coluna;
+		temValor = false;
+		
+	}
+	
+	public Token(TokenType tipo, int linha, int coluna, Object valor) {
+		this.tipo = tipo;
+		this.linha = linha;
+		this.coluna = coluna;
+		this.valor = valor;
+		temValor = true;
+	}
+	
+	
+	public enum TokenType {
+		IDENTIFIER, INTEGER_LITERAL, STRING_LITERAL, 
+		ABSTRACT, PRIVATE, PACKAGE, STRICTFP, CONTINUE, VOLATILE, 
+		TRANSIENT, INTERFACE, PROTECTED, INSTANCEOF, IMPLEMENTS, 
+		SYNCHRONIZED, FINALLY, DEFAULT, EXTENDS, BOOLEAN, PUBLIC, 
+		DOUBLE, IMPORT, NATIVE, RETURN, THROWS, SWITCH, STATIC, 
+		ASSERT, WHILE, FINAL, FLOAT, CLASS, CONST, CATCH, THROW, 
+		SUPER, SHORT, BREAK, VOID, GOTO, LONG, ENUM, ELSE, CHAR, 
+		CASE, THIS, BYTE, FOR, INT, NEW, TRY, DO, IF, IGUAL, MAIS, 
+		IGUALS, EOF, BOOLEAN_LITERAL, NULL_LITERAL, 
+		
+	}
 
 }
+
+
